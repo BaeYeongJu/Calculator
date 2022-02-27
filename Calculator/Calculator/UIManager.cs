ca1f1f170cbd3ca8267d59c3ed70ff5acc338ec2
yeongjuBae <wnqo925@gmail.com>
2022-02-27 22:04:07 +0900

    Input method 추가

---
+++ Calculator/Calculator/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    class UIManager
+    {
+        private double currentValue = 0;
+        private int decimalPointcount = 0;
+        private bool isDecimalPoint = false;
+
+        public double Input(int number)
+        {
+            if (!isDecimalPoint)
+            {
+                currentValue = currentValue * 10 + number;
+            }
+            else
+            {
+                currentValue = currentValue + Math.Pow(10,-1*decimalPointcount) * number;
+                decimalPointcount++;
+            }
+
+            return currentValue;
+        }
+    }
+}
