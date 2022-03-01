85ec99f3bb3236cc3666ad63de4c3eb9cd8b9230
yeongjuBae <wnqo925@gmail.com>
2022-03-01 14:47:46 +0900

    ComputeManager 추가 및 메서드 추가중..

---
+++ Calculator/Calculator/ComputeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+namespace Calculator
+{
+    class ComputeManager
+    {
+        public ComputeManager()
+        {
+            Trace.WriteLine("ComputeManager");
+        }
+        public void Percent() { }
+        public void Delete() { }
+        public void DeleteAll() { }
+        public void Clear() { }
+
+        public double Add(double firstValue, double secondValue) { return firstValue + secondValue; }
+        public double Subtract(double firstValue, double secondValue) { return firstValue - secondValue; }
+        public double Multiply(double firstValue, double secondValue) { return firstValue * secondValue; }
+        public double Divide(double firstValue, double secondValue) { return firstValue / secondValue; }
+        public void Result() { }
+
+
+        public void DecimalPoint() { }
+        public void Mathematics() { }
+        public void PlusOnMinus() { }
+
+
+        public void Reciprocal() { }
+        public void SquareRoot() { }
+        public void Root() { }
+    }
+}
