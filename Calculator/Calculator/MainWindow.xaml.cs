ffcfa974c4fa2b2b59d513e87a8746549e9a522a
yeongjuBae <wnqo925@gmail.com>
2022-03-01 20:34:51 +0900

    ButtonNumberClick 함수에 호출할 NumberButtonClicked 추가

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -38,6 +38,29 @@ namespace Calculator
         private void ButtonNumberClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
+            if (button == null)
+                MessageBox.Show("ButtonNumberClick null");
+
+            if (button == OneButton)
+                uiManager.NumberButtonClicked(1);
+            else if (button == TwoButton)
+                uiManager.NumberButtonClicked(2);
+            else if (button == ThreeButton)
+                uiManager.NumberButtonClicked(3);
+            else if (button == FourButton)
+                uiManager.NumberButtonClicked(4);
+            else if (button == FiveButton)
+                uiManager.NumberButtonClicked(5);
+            else if (button == SixButton)
+                uiManager.NumberButtonClicked(6);
+            else if (button == SevenButton)
+                uiManager.NumberButtonClicked(7);
+            else if (button == EigthtButton)
+                uiManager.NumberButtonClicked(8);
+            else if (button == NineButton)
+                uiManager.NumberButtonClicked(9);
+            else if (button == ZeroButton)
+                uiManager.NumberButtonClicked(0);
         }
 
         public void SetOutputText(string data)
