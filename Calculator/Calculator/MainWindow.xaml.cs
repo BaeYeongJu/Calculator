4575b731a57151e1ef248b418243acdc0f4cd7c3
yeongjuBae <wnqo925@gmail.com>
2022-03-01 19:53:36 +0900

    버튼 UI 처리

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -21,55 +21,63 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private UIManager UIManager;
+        private UIManager uiManager;
       
         public MainWindow()
         {
             InitializeComponent();
-            UIManager = new UIManager();
-            UIManager.Window = this;
+            uiManager = new UIManager();
+            uiManager.Window = this;
         }
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
             if (button == DivideButton)
-                UIManager.FunctionButtonClicked("/");
+                uiManager.FunctionButtonClicked("/");
             else if (button == MultiplyButton)
-                UIManager.FunctionButtonClicked("*");
+                uiManager.FunctionButtonClicked("*");
             else if (button == SubtractButton)
-                UIManager.FunctionButtonClicked("-");
+                uiManager.FunctionButtonClicked("-");
             else if (button == AddButton)
-                UIManager.FunctionButtonClicked("+");
+                uiManager.FunctionButtonClicked("+");
             else if (button == ResultButton)
-                UIManager.FunctionButtonClicked("=");
+                uiManager.FunctionButtonClicked("=");
             else if (button ==DecimalPointButton)
-                UIManager.FunctionButtonClicked(".");
+                uiManager.FunctionButtonClicked(".");
+            else if (button == ClearButton)
+                uiManager.FunctionButtonClicked("C");
+            else if (button == ClearAllButton)
+                uiManager.FunctionButtonClicked("CE");
+            else if (button == DeleteButton)
+                uiManager.FunctionButtonClicked("Delete");
+            else if (button == PlusOnMinus)
+                uiManager.FunctionButtonClicked("+/-");
         }
 
         private void ButtonNumberClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
             if (button == OneButton)
-                UIManager.NumberButtonClicked(1);
+                uiManager.NumberButtonClicked(1);
             else if (button == TwoButton)
-                UIManager.NumberButtonClicked(2);
+                uiManager.NumberButtonClicked(2);
             else if (button == ThreeButton)
-                UIManager.NumberButtonClicked(3);
+                uiManager.NumberButtonClicked(3);
             else if (button == FourButton)
-                UIManager.NumberButtonClicked(4);
+                uiManager.NumberButtonClicked(4);
             else if (button == FiveButton)
-                UIManager.NumberButtonClicked(5);
+                uiManager.NumberButtonClicked(5);
             else if (button == SixButton)
-                UIManager.NumberButtonClicked(6);
+                uiManager.NumberButtonClicked(6);
             else if (button == SevenButton)
-                UIManager.NumberButtonClicked(7);
+                uiManager.NumberButtonClicked(7);
             else if (button == EigthtButton)
-                UIManager.NumberButtonClicked(8);
+                uiManager.NumberButtonClicked(8);
             else if (button == NineButton)
-                UIManager.NumberButtonClicked(9);
+                uiManager.NumberButtonClicked(9);
             else if (button == ZeroButton)
-                UIManager.NumberButtonClicked(0);
+                uiManager.NumberButtonClicked(0);
         }
 
         public void SetOutputText(string data)
