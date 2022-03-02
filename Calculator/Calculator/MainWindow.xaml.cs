37249ab9013ee89d1a38386abcad77dc86045791
yeongjuBae <wnqo925@gmail.com>
2022-03-02 21:25:15 +0900

    ButtonClick 클릭시 numberButtonDictionary의 value 값 전달

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -21,11 +21,23 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         private UIManager uiManager;
+        private Dictionary<Button, int> numberButtonDictionary = new Dictionary<Button, int>();
         public MainWindow()
         {
             InitializeComponent();
             uiManager = new UIManager();
             uiManager.Window = this;
+
+            numberButtonDictionary.Add(ZeroButton, 0);
+            numberButtonDictionary.Add(OneButton, 1);
+            numberButtonDictionary.Add(TwoButton, 2);
+            numberButtonDictionary.Add(ThreeButton, 3);
+            numberButtonDictionary.Add(FourButton, 4);
+            numberButtonDictionary.Add(FiveButton, 5);
+            numberButtonDictionary.Add(SixButton, 6);
+            numberButtonDictionary.Add(SevenButton, 7);
+            numberButtonDictionary.Add(EigthtButton, 8);
+            numberButtonDictionary.Add(NineButton, 9);
         }
 
         private void ButtonClick(object sender, RoutedEventArgs e)
@@ -40,27 +52,7 @@ namespace Calculator
             Button button = sender as Button;
             if (button == null)
                 MessageBox.Show("ButtonNumberClick null");
-
-            if (button == OneButton)
-                uiManager.NumberButtonClicked(1);
-            else if (button == TwoButton)
-                uiManager.NumberButtonClicked(2);
-            else if (button == ThreeButton)
-                uiManager.NumberButtonClicked(3);
-            else if (button == FourButton)
-                uiManager.NumberButtonClicked(4);
-            else if (button == FiveButton)
-                uiManager.NumberButtonClicked(5);
-            else if (button == SixButton)
-                uiManager.NumberButtonClicked(6);
-            else if (button == SevenButton)
-                uiManager.NumberButtonClicked(7);
-            else if (button == EigthtButton)
-                uiManager.NumberButtonClicked(8);
-            else if (button == NineButton)
-                uiManager.NumberButtonClicked(9);
-            else if (button == ZeroButton)
-                uiManager.NumberButtonClicked(0);
+            uiManager.NumberButtonClicked(numberButtonDictionary[button]);
         }
 
         public void SetOutputText(string data)
