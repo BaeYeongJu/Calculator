090ebc8077846e31d689754b17e3c93106a7e97c
yeongjuBae <wnqo925@gmail.com>
2022-03-02 21:16:50 +0900

    기능 버튼 dictionary로 변경 (functionButtonDictionary)

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -22,64 +22,50 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         private UIManager uiManager;
-      
+        private Dictionary<Button, int> numberButtonDictionary = new Dictionary<Button, int>();
+        private Dictionary<Button, string> functionButtonDictionary = new Dictionary<Button, string>();
         public MainWindow()
         {
             InitializeComponent();
             uiManager = new UIManager();
             uiManager.Window = this;
+
+            numberButtonDictionary.Add(ZeroButton, 0);
+            numberButtonDictionary.Add(OneButton, 1);
+            numberButtonDictionary.Add(TwoButton, 2);
+            numberButtonDictionary.Add(ThreeButton, 3);
+            numberButtonDictionary.Add(FourButton, 4);
+            numberButtonDictionary.Add(FiveButton, 5);
+            numberButtonDictionary.Add(SixButton, 6);
+            numberButtonDictionary.Add(SevenButton, 7);
+            numberButtonDictionary.Add(EigthtButton, 8);
+            numberButtonDictionary.Add(NineButton, 9);
+
+            functionButtonDictionary.Add(DivideButton, "/");
+            functionButtonDictionary.Add(MultiplyButton, "*");
+            functionButtonDictionary.Add(SubtractButton, "-");
+            functionButtonDictionary.Add(AddButton, "+");
+            functionButtonDictionary.Add(ResultButton, "=");
+            functionButtonDictionary.Add(DecimalPointButton, ".");
+            functionButtonDictionary.Add(ClearButton, "C");
+            functionButtonDictionary.Add(ClearAllButton, "CE");
+            functionButtonDictionary.Add(DeleteButton, "Delete");
+            functionButtonDictionary.Add(PlusOnMinus, "+/-");
         }
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-            if (button == DivideButton)
-                uiManager.FunctionButtonClicked("/");
-            else if (button == MultiplyButton)
-                uiManager.FunctionButtonClicked("*");
-            else if (button == SubtractButton)
-                uiManager.FunctionButtonClicked("-");
-            else if (button == AddButton)
-                uiManager.FunctionButtonClicked("+");
-            else if (button == ResultButton)
-                uiManager.FunctionButtonClicked("=");
-            else if (button ==DecimalPointButton)
-                uiManager.FunctionButtonClicked(".");
-            else if (button == ClearButton)
-                uiManager.FunctionButtonClicked("C");
-            else if (button == ClearAllButton)
-                uiManager.FunctionButtonClicked("CE");
-            else if (button == DeleteButton)
-                uiManager.FunctionButtonClicked("Delete");
-            else if (button == PlusOnMinus)
-                uiManager.FunctionButtonClicked("+/-");
+            uiManager.FunctionButtonClicked(functionButtonDictionary[button]);
         }
 
         private void ButtonNumberClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-            if (button == OneButton)
-                uiManager.NumberButtonClicked(1);
-            else if (button == TwoButton)
-                uiManager.NumberButtonClicked(2);
-            else if (button == ThreeButton)
-                uiManager.NumberButtonClicked(3);
-            else if (button == FourButton)
-                uiManager.NumberButtonClicked(4);
-            else if (button == FiveButton)
-                uiManager.NumberButtonClicked(5);
-            else if (button == SixButton)
-                uiManager.NumberButtonClicked(6);
-            else if (button == SevenButton)
-                uiManager.NumberButtonClicked(7);
-            else if (button == EigthtButton)
-                uiManager.NumberButtonClicked(8);
-            else if (button == NineButton)
-                uiManager.NumberButtonClicked(9);
-            else if (button == ZeroButton)
-                uiManager.NumberButtonClicked(0);
+            if (button == null)
+                MessageBox.Show("ButtonNumberClick null");
+            uiManager.NumberButtonClicked(numberButtonDictionary[button]);
         }
-
         public void SetOutputText(string data)
         {
             OutputText.Text = data;
