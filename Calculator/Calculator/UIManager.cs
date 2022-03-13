43e06f83aca1c7d38be44bcd753a3ca82c5cbd29
yeongjuBae <wnqo925@gmail.com>
2022-03-13 20:12:21 +0900

    더하기 기능 수정
    - 3+2 = + -> 7+,7이 나옴 (5가 되어야 함) -> 수정함
    - 3+2 + = -> 5+0= ,5가 나옴( 5+5 = 10,10이 나와야 함.) -> 진행중

--- Calculator/Calculator/UIManager.cs
+++ Calculator/Calculator/UIManager.cs
@@ -11,7 +11,7 @@ namespace Calculator
        Plus,
        Minus,
        Multiply,
-       Result,
+       Equal,
        Negate,
        Clear,
        ClearAll,
@@ -36,6 +36,8 @@ namespace Calculator
         private bool isResultClicked = false; //= 클릭했니?
         private bool isFunctionClicked = false; //function 기능 클릭했니?
 
+        private int addCount = 0;
+
         public UIManager()
         {
             Trace.WriteLine("UIManager");
@@ -49,6 +51,7 @@ namespace Calculator
 
             if (stringValue == ".")
             {
+                Trace.WriteLine($".");
                 isDecimalPoint = true;
             }
             else if (stringValue == "/")
@@ -65,36 +68,39 @@ namespace Calculator
             }
             else if (stringValue == "+")
             {
-                if(lastOperator == LastOperator.Result)
+                if (isResultClicked)
                 {
-                    Trace.WriteLine($"LastOperator.Result: {beforeValue}");
-                    Window.SetComputeText("+" + beforeValue.ToString());
-                    currentValue = 0;
-                    lastOperator = LastOperator.Plus;
+                    beforeValue = currentValue;
+                    Trace.WriteLine($"+ LastOperator.Equal : {beforeValue}");
+                    Window.SetComputeText("+" + beforeValue.ToString("0.################"));
                 }
                 else
                 {
-                    lastOperator = LastOperator.Plus;
                     currentValue = computeManager.Add(beforeValue, currentValue);
                     beforeValue = currentValue;
+                    Trace.WriteLine($"+ decimalPointcount: {decimalPointcount}");
                     Window.SetComputeText("+" + currentValue.ToString());
                     Window.SetOutputText(currentValue.ToString());
-                    currentValue = 0;
                 }
-                Trace.WriteLine($"lastOperator: {lastOperator}");
+
+                Trace.WriteLine($"+ beforeValue: {beforeValue}");
+                currentValue = 0;
+                lastOperator = LastOperator.Plus;
+                isDecimalPoint = false;
+                decimalPointcount = 1;
+                addCount++;
             }
             else if(stringValue == "=")
             {
-                
                 if (lastOperator == LastOperator.Plus)
                 {
-                    Trace.WriteLine($"beforeValue: {beforeValue}");
-                    Trace.WriteLine($"currentValue: {currentValue}");
+                    Trace.WriteLine($"= beforeValue: {beforeValue}");
+                    Trace.WriteLine($"= currentValue: {currentValue}");
                     string result = beforeValue.ToString() + "+" + currentValue.ToString();
                     Window.SetComputeText("="+result);
-                    currentValue = computeManager.Add(beforeValue, currentValue);
-                    beforeValue = currentValue;
-                    Window.SetOutputText(currentValue.ToString());
+                    //currentValue 
+                    beforeValue = computeManager.Add(beforeValue, currentValue);
+                    Window.SetOutputText(beforeValue.ToString());
                 }
                 else if (lastOperator == LastOperator.Minus)
                     currentValue = computeManager.Subtract(beforeValue, currentValue);
@@ -103,9 +109,10 @@ namespace Calculator
                 else if (lastOperator == LastOperator.Division)
                     currentValue = computeManager.Divide(beforeValue, currentValue);
                 else
-                    Window.SetComputeText("="+currentValue.ToString());
+                    Window.SetComputeText("="+currentValue.ToString("0.################"));
 
-                lastOperator = LastOperator.Result;
+                isResultClicked = true;
+                //lastOperator = LastOperator.Equal;
             }
             else if (stringValue == "+/-")
             {
@@ -114,17 +121,11 @@ namespace Calculator
             }
             else if (stringValue == "C")
             {
-                isOperatorClicked = false;
-                isNumberClicked = false;
-                currentValue = 0;
-                beforeValue = 0;
-                Window.SetComputeText(string.Empty);
-                Window.SetOutputText("0");
-                lastOperator = LastOperator.None;
+                Clear();
             }
             else if (stringValue == "CE")
             {
-
+                
             }
             else if (stringValue == "Delete")
             {
@@ -140,6 +141,12 @@ namespace Calculator
             if (!isOperatorClicked && !isNumberClicked)
                 isNumberClicked = true;
 
+            if(isResultClicked)
+            {
+                //초기화 (중복 부분)
+                Clear();
+            }
+
             if (!isDecimalPoint)
             {
                 currentValue = currentValue * 10 + number;
@@ -149,11 +156,40 @@ namespace Calculator
                 currentValue = currentValue + Math.Pow(10, -1 * decimalPointcount) * number;
                 decimalPointcount++;
             }
-
             string stringformat = "{0:N" + (decimalPointcount - 1) + "}";
-            Window.SetOutputText(string.Format(stringformat, currentValue));
 
+            if (isDecimalPoint)
+                currentValue = double.Parse(string.Format(stringformat, currentValue));
+
+            Window.SetOutputText(currentValue.ToString("0.################")); //"0.################"
+
+            Trace.WriteLine($"num currentValue: {currentValue}");
+            Trace.WriteLine($"num beforeValue: {beforeValue}");
             Trace.WriteLine($"isOperatorClicked:{isOperatorClicked} ,isNumberClicked:{isNumberClicked}");
         }
+
+        private void Clear()
+        {
+            isDecimalPoint = false;
+            decimalPointcount = 1;
+            isOperatorClicked = false;
+            isNumberClicked = false;
+            currentValue = 0;
+            beforeValue = 0;
+            Window.SetComputeText(string.Empty);
+            Window.SetOutputText("0");
+            lastOperator = LastOperator.None;
+            isResultClicked = false;
+            addCount = 0;
+        }
+    }
+}
+
+public static class ExtensionClass
+{
+    public static double ToStiringFormat(double currnentValue, int decimalPointCount)
+    {
+        string stringformat = "{0:N" + (decimalPointCount - 1) + "}";
+        return double.Parse(string.Format(stringformat, currnentValue));
     }
 }
